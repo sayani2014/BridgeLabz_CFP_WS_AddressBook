ae74fa8060e10d4a7d271178dbd130545306e963
Sayani Koley <sayanikoley8@gmail.com>
2021-08-14 08:42:51 +0530

    Create a Address Book Spring Project to cater to REST Request.

---
+++ addressbook/src/main/java/com/bridgelabz/sayani/addressbook/AddressBookApplication.java
@@ -0,0 +1,20 @@
+/**
+ * UC1 : Create a Address Book Spring Project to cater to REST Request.
+ *
+ * @author : SAYANI KOLEY
+ * @since : 14.08.2021
+ */
+
+package com.bridgelabz.sayani.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApplication.class, args);
+	}
+
+}
